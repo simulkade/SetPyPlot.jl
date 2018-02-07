1dc18d66bc97c51fe791be1de3ac3ebb06ec2662
AAE <e.eftekhari@gmail.com>
2018-02-07 14:35:33 +0100

    first commit

---
+++ src/SetPyPlot.jl
@@ -0,0 +1,32 @@
+module SetPyPlot
+
+# setting pyplot parameters to default values
+using PyPlot
+
+"""
+setrcparam(;tick_font_size = 10,
+legend_font_size = 10,
+axis_label_font_size = 10,
+fig_width = 8,
+fig_height = 6)
+
+sets the font size parameters to my favorite default values
+"""
+function setrcparam(;tick_font_size = 10,
+    legend_font_size = 10,
+    axis_label_font_size = 10,
+    fig_width = 8,
+    fig_height = 6)
+
+    rc("xtick", labelsize=tick_font_size)
+    rc("ytick", labelsize=tick_font_size)
+    rc("legend", fontsize=legend_font_size)
+    rc("axes", labelsize=axis_label_font_size)
+    rc("figure", dpi = 300)
+    rc("figure", figsize = (fig_width, fig_height)./2.54)
+    rc("savefig", bbox = "tight")
+end
+
+# more to come later...
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using PyPlot, SetPyPlot
+using Base.Test
+
+# write your own tests here
+figure(1)
+
+x = collect(linspace(0, 2π))
+y1 = sin(x)
+plot(x, sin(x), label = "sin(x)")
+plot(x, cos(x), label = "cos(x)")
+xlabel("x values")
+ylabel("y values")
+legend()
+tight_layout()
+
+gcf() # to plot in vscode
+
+# savefig("publication_test.png")
+
+@test 1==1
